01c891c75830d51decbc023cca437e5d906eeffb
Nicolas.N <nicolasnguyen0023@hotmail.com>
2022-11-17 14:25:24 +0100

    watch list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Movie.destroy_all
+
+1000.times do
+  movie_title = Faker::Movie.title
+  movie_overview = Faker::Movie.quote
+  movie_url = Faker::Internet.url
+  movie_rating = rand(0..5)
+
+  Movie.create(
+    title: movie_title,
+     overview: movie_overview,
+      poster_url: movie_url,
+      rating: movie_rating)
+end
